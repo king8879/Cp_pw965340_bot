bdeb69fc58badbdd008ce69ad16cfc1125ba25a7
king8879 <k4507244@gmail.com>
2025-04-28 23:38:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "22923037"
+api_hash = "dfb3666878b3851460a58461c5a50f5b"
+bot_token = "7985324122:AAH2PDRkpZYZRMB7cmB2QvGj06Gy9cayaUo"
+auth_users = [6554343173]
